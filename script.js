ab9bb17137af722fbee3065c6d66c9eb2010e577
Mikael <mantism@seas.upenn.edu>
2016-04-20 14:18:02 -0400

    added menu button

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 $(document).ready(function() {
-      var map = new Datamap({
-            element: document.getElementById('map-container'),
-            projection: 'mercator'
+
+
+
+});
+
+$(function() {
+      $('#menuButton').button({
+            icons: {primary: null},
+            text: false
+      }).addClass('ButtonClass').append('<img height="25" src="res/menu.png" width="25" />' ).button();
+      $('#menuButton').click(function() {
+            $('#myScrollspy').toggle("fast",             function(){
+                  $('#myScrollspy').css('right',0);
+            });
       });
-
-
-
 
 });
 
@@ -17,13 +25,13 @@ $(document).ready(function() {
             {label: 'Female', count: 11}
       ];
 
-      var width = 360;
-      var height = 360;
-      var radius = Math.min(width, height) / 2;
+      var width = window.innerWidth;
+      var height = 500;
+      var radius = 200;
 
       var color = d3.scale.ordinal().range(["#4f98d3", "#e73a3a"]);
 
-      var svg = d3.select('#chart').append('svg').attr('width', width).attr('height', height).append('g').attr('transform', 'translate(' + (width/2) +  ',' + (height/2) + ')');
+      var svg = d3.select('#chart').append('svg').attr('width', width).attr('height', height).append('g').attr('transform', 'translate(' + (width/3) +  ',' + (height/2) + ')');
 
       var arc = d3.svg.arc().outerRadius(radius);
       var pie = d3.layout.pie().value(function(d) { return d.count; }).sort(null);
