fa8faba0b06453f7d04398646c28fb55d8a453ed
Mikael <mantism@seas.upenn.edu>
2017-08-03 03:19:28 -0400

    adds create-react app and header components/styles

---
+++ tricking/src/Header.jsx
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import Icons from './Icons';
+class Header extends Component {
+  render() {
+    return (
+      <div className="header">
+        <h1>Tricking Survey Results</h1>
+        <h3>2016</h3>
+        <Icons />
+      </div>
+    );
+  }
+}
+
+export default Header;
