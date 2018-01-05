0b54ebbb8578b9bd3092dd1f32cdbd5034e54969
Mikael <mantism@seas.upenn.edu>
2018-01-05 15:33:32 -0500

    minor edit: updates size of tricking fb page

--- src/About.jsx
+++ src/About.jsx
@@ -16,7 +16,7 @@ class About extends Component {
 			      However, tricking isn't just defined by its moves, its defined by the community that practices it.<br/><br/>
             At the time, the Tricking facebook group contained <span className="highlight"> 23,155 </span> members.
             However, shortly after the survey was sent the original group was made secret and is now no longer active.
-            From its ashes came a new Facebook group that now contains <span className="highlight"> 10,000+ </span>
+            From its ashes came a new Facebook group that now contains <span className="highlight"> 11,000+ </span>
             trickers and is much more active, well-maintained, and steadily growing. <br/> <br/> 
             Out of this old facebook group, <span className="highlight"> 365 </span> trickers answered the call, responding to questions such as <i>"How long have you been tricking?"</i>, <i>"How often do you 
             train"</i>, <i>"What is your favorite trick?"</i>, and much more. <br/><br/>
